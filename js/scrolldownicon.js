ae326dac5dcd0cd378f9964f6320b59d97f0e31d
pochu4 <159850731+pochu4@users.noreply.github.com>
2025-02-02 18:28:38 -0800

    aboutpage, scrollbutton, hero fix

--- js/scrolldownicon.js
+++ js/scrolldownicon.js
@@ -1,8 +1,19 @@
-
 gsap.to(".scrolldown-icon ion-icon", {
     y: -10,
     duration: 0.5,
     yoyo: true,
     repeat: -1,
     ease: "power1.inOut"
+});
+
+// Register scroll to plugin
+gsap.registerPlugin(ScrollToPlugin);
+
+
+document.getElementById("scroll-down").addEventListener("click", function () {
+    gsap.to(window, {
+        duration: 1, 
+        scrollTo: "#next",
+        ease: "power2.inOut" 
+    });
 });
